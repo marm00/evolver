3b6a7ec7da9b7860a9e543e1173e7f8b1adf2f94
marm00 <markscheperman@gmail.com>
2024-06-05 22:42:07 +0200

    simple movement

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Player from './components/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div style={{ position: 'relative', width: '100%', height: '100vh', backgroundColor: '#87CEEB' }}>
+            <Player />
+        </div>
+    );
+};
 
 export default App;
