7bf83ed3c289b8080586fb924c2333ffe8f7ffc1
marm00 <markscheperman@gmail.com>
2024-07-15 10:20:28 +0200

    deployment testing prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
           >
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
+              (TEST) Just the basics - Everything you need to know to set up your
               database and authentication.
             </div>
           </Link>
