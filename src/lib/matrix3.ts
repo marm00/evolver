735c69b72bc5ab15b44cda2cab4a4500851d86a5
marm00 <markscheperman@gmail.com>
2024-07-31 10:44:42 +0200

    set clone premul translate _b

--- src/lib/matrix3.ts
+++ src/lib/matrix3.ts
@@ -1,18 +1,25 @@
 import { type Resettable } from "./pool";
+import { type Vector2 } from "./vector2";
 
+/**
+ * A 3x3 matrix with [column-major ordering](https://en.wikipedia.org/wiki/Row-_and_column-major_order):
+ * 
+ * E.g. `a12` is the element in the first row and second column, indexable as `this.elements[3]`.
+ * 
+ * >```
+ * a11  |> a12  |> a13  ==  0  |> 3  |> 6  
+ * a21  || a22  || a23  ==  1  || 4  || 7
+ * a31  >^ a32  >^ a33  ==  2  >^ 5  >^ 8
+ * >```
+ */
 class Matrix3 implements Resettable {
     elements: Float32Array;
 
     /** 
-     * Constructs a new 3x3 matrix with [column-major ordering](https://en.wikipedia.org/wiki/Row-_and_column-major_order):
-     * 
-     * >```
-     * a11  |> a12  |> a13  ==  0  |> 3  |> 6  
-     * a21  || a22  || a23  ==  1  || 4  || 7
-     * a31  >^ a32  >^ a33  ==  2  >^ 5  >^ 8
-     * >```
+     * Constructs a new 3x3 matrix with [column-major ordering](https://en.wikipedia.org/wiki/Row-_and_column-major_order).
      */
     constructor(
+        //   row 1 ,      row 2 ,      row 3 ,
         a11: number, a21: number, a31: number, // col 1
         a12: number, a22: number, a32: number, // col 2
         a13: number, a23: number, a33: number  // col 3
@@ -25,6 +32,15 @@ class Matrix3 implements Resettable {
         a[6] = a13, a[7] = a23, a[8] = a33; // col 3
     }
 
+    /** Constructs and returns a new [identity matrix](https://en.wikipedia.org/wiki/Identity_matrix). */
+    static identity(this: void): Matrix3 {
+        return new Matrix3(
+            1, 0, 0, // col 1
+            0, 1, 0, // col 2
+            0, 0, 1, // col 3
+        );
+    }
+
     /** Resets the matrix to the [identity matrix](https://en.wikipedia.org/wiki/Identity_matrix). */
     reset(): void {
         const a = this.elements;
@@ -34,7 +50,30 @@ class Matrix3 implements Resettable {
         a[6] = 0, a[7] = 0, a[8] = 1; // col 3
     }
 
-    // TODO: translate, rotate, scale, TRS, vector mul, maybe a 2x3 matrix function (dont need last row often)
+    /** Sets the matrix elements with [column-major ordering](https://en.wikipedia.org/wiki/Row-_and_column-major_order). */
+    set(//r1           r2           r3
+        a11: number, a21: number, a31: number, // col 1
+        a12: number, a22: number, a32: number, // col 2
+        a13: number, a23: number, a33: number  // col 3
+    ): this {
+        const a = this.elements;
+        //1   row ,   2   row ,   3   row ;
+        a[0] = a11, a[1] = a21, a[2] = a31; // col 1
+        a[3] = a12, a[4] = a22, a[5] = a32; // col 2
+        a[6] = a13, a[7] = a23, a[8] = a33; // col 3
+        return this;
+    }
+
+    clone(): Matrix3 {
+        const a = this.elements;
+        return new Matrix3(
+            //1r ,   2r ,   3r ,
+            a[0]!, a[1]!, a[2]!, // column 1
+            a[3]!, a[4]!, a[5]!, // column 2
+            a[6]!, a[7]!, a[8]!  // column 3
+        );
+    }
+
 
     /** 
      * Matrix multiplication in place. Memory layout by indices:
@@ -45,7 +84,7 @@ class Matrix3 implements Resettable {
      * 1 4 7   1 4 7
      * 2 5 8   2 5 8
      * >```
-     */
+    */
     mul(m: Matrix3): this {
         const a = this.elements;
         const b = m.elements;
@@ -60,7 +99,6 @@ class Matrix3 implements Resettable {
         const b21 = b[1]!, b22 = b[4]!, b23 = b[7]!; // row 2
         const b31 = b[2]!, b32 = b[5]!, b33 = b[8]!; // row 3
 
-        // Set `this` mul results in column-major order col1=(0,1,2) col2=(3,4,5) col3=(6,7,8)
         a[0] = a11 * b11 + a12 * b21 + a13 * b31; // a11
         a[1] = a21 * b11 + a22 * b21 + a23 * b31; // a21
         a[2] = a31 * b11 + a32 * b21 + a33 * b31; // a31
@@ -74,14 +112,67 @@ class Matrix3 implements Resettable {
         return this;
     }
 
-    clone(): Matrix3 {
+    /** 
+    * Inverse matrix multiplication in place. Memory layout by indices:
+    * 
+    * >```
+    * this    m
+    * 0 3 6   0 3 6
+    * 1 4 7   1 4 7
+    * 2 5 8   2 5 8
+    * >```
+   */
+    premul(m: Matrix3): this {
         const a = this.elements;
-        return new Matrix3(
-            //1r ,   2r ,   3r ,
-            a[0]!, a[1]!, a[2]!, // column 1
-            a[3]!, a[4]!, a[5]!, // column 2
-            a[6]!, a[7]!, a[8]!  // column 3
-        );
+        const b = m.elements;
+
+        //        col 1  ,     col 2  ,     col 3  ;
+        const a11 = a[0]!, a12 = a[3]!, a13 = a[6]!; // row 1
+        const a21 = a[1]!, a22 = a[4]!, a23 = a[7]!; // row 2
+        const a31 = a[2]!, a32 = a[5]!, a33 = a[8]!; // row 3
+
+        //        col 1  ,     col 2  ,     col 3  ;
+        const b11 = b[0]!, b12 = b[3]!, b13 = b[6]!; // row 1
+        const b21 = b[1]!, b22 = b[4]!, b23 = b[7]!; // row 2
+        const b31 = b[2]!, b32 = b[5]!, b33 = b[8]!; // row 3
+
+        a[0] = b11 * a11 + b12 * a21 + b13 * a31; // a11
+        a[1] = b21 * a11 + b22 * a21 + b23 * a31; // a21
+        a[2] = b31 * a11 + b32 * a21 + b33 * a31; // a31
+        a[3] = b11 * a12 + b12 * a22 + b13 * a32; // a12
+        a[4] = b21 * a12 + b22 * a22 + b23 * a32; // a22
+        a[5] = b31 * a12 + b32 * a22 + b33 * a32; // a32
+        a[6] = b11 * a13 + b12 * a23 + b13 * a33; // a13
+        a[7] = b21 * a13 + b22 * a23 + b23 * a33; // a23
+        a[8] = b31 * a13 + b32 * a23 + b33 * a33; // a33
+
+        return this;
     }
 
+    // TODO: rotate, scale, TRS, vector mul, maybe a 2x3 matrix function (dont need last row often)
+    translate(v: Vector2): this {
+        _b.set(
+            1, 0, 0,    // col 1
+            0, 1, 0,    // col 2
+            v.x, v.y, 1 // col 3
+        ); 
+        this.premul(_b);
+        return this;
+    }
+
+
 }
+
+/** 
+ * A privately shared Matrix3 for all Matrix3 instances, used for temporary operations like {@link Matrix3.translate}.
+ * Using this architecture over a pool or pool instance parameter is faster and more convenient. The use of
+ * __&#47;&#42;@&#95;&#95;PURE&#95;&#95;&#42;&#47;__ informs the compiler that this variable can be safely removed 
+ * if it goes unused.
+ * 
+ * @see https://webpack.js.org/guides/tree-shaking/
+ */
+const _b = /*@__PURE__*/ new Matrix3(
+    1, 0, 0, // col 1
+    0, 1, 0, // col 2
+    0, 0, 1, // col 3
+);
