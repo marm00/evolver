b7383bfc899acd4a367c1c30dff805d19c8a2fce
marm00 <markscheperman@gmail.com>
2024-08-06 15:31:29 +0200

    fiddle js to ts

---
+++ _fiddle.ts
@@ -0,0 +1,42 @@
+// Unused test file
+
+class A {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static zero() {
+        return new A(0, 0);
+    }
+
+    clone() {
+        return new A(this.x, this.y);
+    }
+}
+
+function staticZeros(iterations: number) {
+    const start = performance.now();
+    for (let i = 0; i < iterations; i++) {
+        const a = A.zero();
+    }
+    const end = performance.now();
+    return end - start;
+}
+
+function cloneZeros(iterations: number) {
+    const start = performance.now();
+    const zero = new A(0, 0);
+    for (let i = 1; i < iterations; i++) {
+        const a = zero.clone();
+    }
+    const end = performance.now();
+    return end - start;
+}
+
+const iterations = 100000000;
+console.log("Static:", staticZeros(iterations), "ms");
+console.log("Clone:", cloneZeros(iterations), "ms");
