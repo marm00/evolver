f82d6ca4066799803e2372b6bfe93ba6a977f86e
marm00 <markscheperman@gmail.com>
2024-12-27 15:04:47 +0100

    better debug select

--- src/app/_components/debug-panel.tsx
+++ src/app/_components/debug-panel.tsx
@@ -2,7 +2,8 @@ import * as game from "~/lib/game";
 
 export function DebugPanel({ gameState, frameCount }: { gameState: game.Game, frameCount: number }) {
     return (
-        <div className="absolute top-0 right-0 p-2 select-none hover:select-text bg-slate-300/25 text-white">
+        <div className="absolute top-0 right-0 p-2 select-none hover:select-text focus:select-text outline-none
+        focus:ring-2 focus:ring-slate-500 bg-slate-300/25 text-white" tabIndex={0}>
             XY: {'(' + gameState.player.center.x.toFixed(1)}, {gameState.player.center.y.toFixed(1) + ')'}
         </div>
     )
