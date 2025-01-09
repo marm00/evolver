ae446b4ea5f1f443d1766c8eaf5d06ee7ee7a53a
marm00 <markscheperman@gmail.com>
2025-01-09 18:21:43 +0100

    lint fix

--- src/app/_components/game.tsx
+++ src/app/_components/game.tsx
@@ -79,6 +79,7 @@ export function Game() {
             }
 
             const handleMouseUp = (_: MouseEvent) => {
+                return;
             }
 
             // TODO: case insensitive actions
